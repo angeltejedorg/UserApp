1a2685f507d76b0029bf5395614529cf4c4e62fc
Angel <angeltejedorg@gmail.com>
2022-11-03 23:12:21 -0500

    Remove unnecesary files

--- Pods/Target Support Files/Pods-PruebaApp-PruebaAppUITests/Pods-PruebaApp-PruebaAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PruebaApp_PruebaAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PruebaApp_PruebaAppUITests
-@end
--- Pods/Target Support Files/Pods-PruebaAppTests/Pods-PruebaAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PruebaAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_PruebaAppTests
-@end
